6177de40fa7b479907d880f8e0000ff06c608229
xavier <fxt@bu.edu>
2024-12-05 23:31:31 -0500

    met requirements

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify, send_from_directory
 import numpy as np
 import nltk
 import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.metrics.pairwise import euclidean_distances
 from PIL import Image
 import open_clip
 from open_clip import create_model_and_transforms, tokenizer
@@ -16,6 +18,35 @@ print("LOADED ! Now creating model.")
 model, _, preprocess = create_model_and_transforms('ViT-B/32', pretrained='openai')
 tokenizer = open_clip.get_tokenizer('ViT-B-32')
 model.eval()
+print("DONE! Now loading images for PCA")
+def load_images(image_dir="coco_images_resized", max_images=None, target_size=(224, 224)):
+    images = []
+    image_names = []
+    for i, filename in enumerate(os.listdir(image_dir)):
+        if filename.endswith('.jpg'):
+            img = Image.open(os.path.join(image_dir, filename))
+            img = img.convert('L')  # Convert to grayscale ('L' mode)
+            img = img.resize(target_size)  # Resize to target size
+            img_array = np.asarray(img, dtype=np.float32) / 255.0  # Normalize pixel values to [0, 1]
+            images.append(img_array.flatten())  # Flatten to 1D
+            image_names.append(filename)
+        if max_images and i + 1 >= max_images:
+            break
+    return np.array(images), image_names
+
+def nearest_neighbors(query_embedding, embeddings, top_k=5):
+    distances = euclidean_distances(query_embedding.reshape(1, -1), embeddings).flatten()
+    nearest_indices = np.argsort(distances)[:top_k]
+    
+    return nearest_indices, distances
+
+train_images, train_image_names = load_images(max_images=2000, target_size=(224, 224))
+flat_images = np.array([img.flatten() for img in train_images])
+print(f"Loaded {len(train_images)} images for PCA training.")
+transform_images, transform_image_names = load_images(max_images=10000, target_size=(224, 224))
+transform_images_flat = np.array([img.flatten() for img in transform_images[:10000]])
+print(f"Loaded {len(transform_images)} images for transformation.")
+
 print("Done! Now opening flask application.")
 app = Flask(__name__)
 
@@ -35,7 +66,8 @@ def search():
     hybrid_weight = request.form.get('hybridWeight')
     query_type = request.form.get('queryType')
     image_file = request.files.get('imageQuery') 
-
+    pcaK = request.form.get('pcaK')
+    embed_type = request.form.get('embedType')
     if query_type == "text-query":
         text = tokenizer([queryrec])
         query = F.normalize(model.encode_text(text)).detach().numpy()
@@ -54,17 +86,41 @@ def search():
         text_query = F.normalize(model.encode_text(text))
         lam  = float(hybrid_weight)
         query = F.normalize(lam * text_query + (1.0 - lam) * image_query).detach().numpy()
-    
-    cosine_similarities = np.dot(normalized_embeddings, query.T).squeeze()
-    top_indices = np.argsort(cosine_similarities)[-5:][::-1] 
 
-    top_images = [
-    {
-        'path': f"/images/{df.iloc[index]['file_name']}",
-        'similarity': float(cosine_similarities[index])
-    }
-    for index in top_indices
-    ]
+    print(pcaK, embed_type)
+    if query_type == "image-query" and embed_type == "pca":
+        print("pca!")
+        k = int(pcaK)
+        pca = PCA(n_components=k)
+        pca.fit(transform_images_flat)
+        reduced_embeddings = pca.transform(transform_images_flat)
+        
+        img = Image.open(image_file.stream)
+        img = img.convert('L')  # Convert to grayscale
+        img = img.resize((224, 224))  # Resize
+        qimage = (np.asarray(img, dtype=np.float32) / 255.0).flatten()
+        #not actually cosine similarities but to keep the same variable name when using pca or clip
+        top_indices, cosine_similarities = nearest_neighbors(pca.transform(qimage.reshape(1,-1)), reduced_embeddings)
+
+        top_images = [
+        {
+            'path': f"/images/{transform_image_names[index]}",
+            'similarity': float(cosine_similarities[index])
+        }
+        for index in top_indices
+        ]
+        
+    else:
+        cosine_similarities = np.dot(normalized_embeddings, query.T).squeeze()
+        top_indices = np.argsort(cosine_similarities)[-5:][::-1] 
+
+        top_images = [
+        {
+            'path': f"/images/{df.iloc[index]['file_name']}",
+            'similarity': float(cosine_similarities[index])
+        }
+        for index in top_indices
+        ]
 
     return jsonify({'message': 'Success', 'query': queryrec, 'hybridWeight': hybrid_weight, 'queryType': query_type, 'topImages': top_images})
 
--- static/script.js
+++ static/script.js
@@ -5,6 +5,8 @@ document.getElementById('search-form').addEventListener('submit', function (even
     let imageQuery = document.getElementById('image-query').files[0]; 
     let hybridWeight = document.getElementById('hybrid-weight').value;
     let queryType = document.getElementById('query-type').value;
+    let pcaK = document.getElementById('pca-k').value;
+    let embedType = document.getElementById('embed-type').value; 
 
     let resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = '';
@@ -13,6 +15,8 @@ document.getElementById('search-form').addEventListener('submit', function (even
     formData.append('query', query);
     formData.append('hybridWeight', hybridWeight);
     formData.append('queryType', queryType);
+    formData.append('pcaK', pcaK); 
+    formData.append('embedType', embedType); 
     
     if (imageQuery) {
         formData.append('imageQuery', imageQuery);
@@ -30,15 +34,20 @@ document.getElementById('search-form').addEventListener('submit', function (even
     
         const flexContainer = document.createElement('div');
         flexContainer.className = 'flex flex-col items-center gap-8';
+
+        console.log("Embed Type:", embedType)
+        const label = embedType === 'pca' ? 'Distance' : 'Similarity';
     
         data.topImages.forEach(image => {
             const imgElement = document.createElement('img');
             imgElement.src = image.path;
-            imgElement.alt = `Similarity: ${image.similarity.toFixed(2)}`;
+            imgElement.alt = `${label}: ${image.similarity.toFixed(2)}`;
             imgElement.className = 'w-128 h-auto rounded shadow-lg';
+
+            
     
             const caption = document.createElement('p');
-            caption.textContent = `Similarity: ${image.similarity.toFixed(2)}`;
+            caption.textContent = `${label}: ${image.similarity.toFixed(2)}`;
             caption.className = 'text-sm text-center';
     
             const container = document.createElement('div');
